9c8c504f4762c59908e7ff7958763e0013459d4d
Ivan Tikhonov <pairbit@gmail.com>
2026-08-31 16:22:54 +0300

    add Prefixed

--- src/StackExchange.Redis/KeyspaceIsolation/KeyPrefixed.cs
+++ src/StackExchange.Redis/KeyspaceIsolation/KeyPrefixed.cs
@@ -419,15 +419,13 @@ namespace StackExchange.Redis.KeyspaceIsolation
             // TODO: The return value could contain prefixed keys. It might make sense to 'unprefix' those?
             Inner.ScriptEvaluateAsync(hash, ToInner(keys), values, flags);
 
-        public Task<RedisResult> ScriptEvaluateMemoryAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<RedisResult> ScriptEvaluateMemoryAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None) =>
+            // TODO: rented args?
+            Inner.ScriptEvaluateMemoryAsync(script, ToInner(args), argsDisposer, flags);
 
-        public Task<Lease<byte>?> ScriptEvaluateMemoryLeaseAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<Lease<byte>?> ScriptEvaluateMemoryLeaseAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None) =>
+            // TODO: rented args?
+            Inner.ScriptEvaluateMemoryLeaseAsync(script, ToInner(args), argsDisposer, flags);
 
         public Task<RedisResult> ScriptEvaluateAsync(string script, RedisKey[]? keys = null, RedisValue[]? values = null, CommandFlags flags = CommandFlags.None) =>
             // TODO: The return value could contain prefixed keys. It might make sense to 'unprefix' those?
@@ -445,15 +443,13 @@ namespace StackExchange.Redis.KeyspaceIsolation
             // TODO: The return value could contain prefixed keys. It might make sense to 'unprefix' those?
             Inner.ScriptEvaluateAsync(hash, ToInner(keys), values, flags);
 
-        public Task<RedisResult> ScriptEvaluateMemoryReadOnlyAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<RedisResult> ScriptEvaluateMemoryReadOnlyAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None) =>
+            // TODO: rented args?
+            Inner.ScriptEvaluateMemoryReadOnlyAsync(script, ToInner(args), argsDisposer, flags);
 
-        public Task<Lease<byte>?> ScriptEvaluateMemoryReadOnlyLeaseAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<Lease<byte>?> ScriptEvaluateMemoryReadOnlyLeaseAsync(string script, ReadOnlyMemory<RedisKeyOrValue> args, IScriptRequestDisposer? argsDisposer, CommandFlags flags = CommandFlags.None) =>
+            // TODO: rented args?
+            Inner.ScriptEvaluateMemoryReadOnlyLeaseAsync(script, ToInner(args), argsDisposer, flags);
 
         public Task<RedisResult> ScriptEvaluateReadOnlyAsync(string script, RedisKey[]? keys = null, RedisValue[]? values = null, CommandFlags flags = CommandFlags.None) =>
             // TODO: The return value could contain prefixed keys. It might make sense to 'unprefix' those?
@@ -965,6 +961,28 @@ namespace StackExchange.Redis.KeyspaceIsolation
                 args = withPrefix;
             }
             return args;
+        }
+
+        protected ReadOnlyMemory<RedisKeyOrValue> ToInner(ReadOnlyMemory<RedisKeyOrValue> outer)
+        {
+            if (outer.Length == 0)
+            {
+                return outer;
+            }
+            else
+            {
+                // TODO: add rent
+                RedisKeyOrValue[] inner = new RedisKeyOrValue[outer.Length];
+
+                var i = 0;
+                foreach (ref readonly var item in outer.Span)
+                {
+                    var key = item.Key;
+                    inner[i++] = key.IsNull ? item : ToInner(key);
+                }
+
+                return inner;
+            }
         }
 
         [return: NotNullIfNotNull("outer")]
