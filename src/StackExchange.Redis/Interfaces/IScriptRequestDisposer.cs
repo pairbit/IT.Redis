8ac2d68018afc3eb3a9a00872e968b29b577de7a
Ivan Tikhonov <pairbit@gmail.com>
2026-08-31 14:01:57 +0300

    add IScriptRequestDisposer

---
+++ src/StackExchange.Redis/Interfaces/IScriptRequestDisposer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StackExchange.Redis;
+
+/// <summary>
+/// Disposing the lua script request data.
+/// </summary>
+public interface IScriptRequestDisposer
+{
+    /// <summary>
+    /// Disposing the lua script request data.
+    /// </summary>
+    /// <param name="args">lua script request.</param>
+    void Dispose(ReadOnlyMemory<RedisKeyOrValue> args);
+}
